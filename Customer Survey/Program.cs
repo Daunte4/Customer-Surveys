0ee91beb58d97db746ebdffabd91d2c80b52694f
Daunte Dottery <d.dottery4@gmail.com>
2019-03-27 08:27:49 -0400

    free

--- Customer Survey/Program.cs
+++ Customer Survey/Program.cs
@@ -1,12 +1,64 @@
 ﻿using System;
+using System.Threading;
 
-namespace Customer_Survey
+namespace Survey
 {
     class Program
     {
+        private static string leave;
+
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+
+            string leave = "";
+            bool check = false;
+            {
+
+
+                Console.WriteLine("What is your name?");
+                var name = Console.ReadLine();
+
+                Console.WriteLine("What is your age?");
+                var age = Console.ReadLine();
+
+                Console.WriteLine("Why do you like Coding?");
+                var coding = Console.ReadLine();
+
+                Console.WriteLine("Your name is: {0}", name);
+                Console.WriteLine("Your age is: {0}", age);
+                Console.WriteLine("Your answer is: {0}", coding);
+
+                Console.WriteLine("=======================================================");
+                Console.WriteLine("");
+                Thread.Sleep(3000);
+                Console.WriteLine("Thanks for Participating");
+                Console.WriteLine("=======================================================");
+
+                Console.WriteLine("\nDo you want to leave yes or no?" + "\n 1.Yes" + " 2. No");
+                Console.WriteLine("\nMy option is: ");
+                leave = Console.ReadLine();
+                switch (leave)
+                {
+                    case "1":
+                        Console.WriteLine("you chose Yes.");
+                        check = true;
+                        break;
+
+                    case "2":
+                        Console.WriteLine("you chose No.");
+                        check = true;
+                        break;
+
+                    default:
+                        Console.WriteLine("invalid choice.");
+                        check = false;
+                        break;
+                }
+            }
+
+
+
         }
+
     }
 }
